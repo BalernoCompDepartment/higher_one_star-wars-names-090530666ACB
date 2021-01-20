61d62578f7755ad57dcac4d501ba10e0e0a770dd
0905306666ACB <090530666@ea.edin.sch.uk>
2021-01-20 15:25:43 +0000

    i wrote the program

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
 class Main {
   public static void main(String[] args) {
     //Step 1: ask user for firstname
+    String forename = Keyboard.getText("Please enter youfirst name");
+
+    String surname = Keyboard.getText("please enter your surname");  //Step 2: ask user for surname
     
-    //Step 2: ask user for surname
-    
+    String mothersMaidenName = Keyboard.getText("please enter your mothers maiden name ");
     //Step 3: ask user for mothers maiden name
-    
+    String city = Keyboard.getText("what city were you born in");
     //Step 4: ask user for city in which they were born
+    
+    String firstNameTwo = forename.substring(0,2).toLowerCase();
+
+    String surnameThree = surname.substring(0,3);
+
+    String cityname = city.substring(0,3);
+
+    String mothername = mothersMaidenName.substring(0,2);
+String starwarsname = surnameThree + firstNameTwo + " " + cityname + mothername ;
     
     //Step 5: starwarsname = first 3 letter of surname + first 2 letters of first name + " " + first 3 letters of motehrs maiden name + first 2 letters of town you **were born in
 
